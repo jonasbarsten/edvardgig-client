8e9b84e482d0dfb3140ff7633a4746b7f9d2ff86
Jonas Barsten <jonasbarsten@gmail.com>
2019-03-15 18:09:18 +0100

    Only loading components when in use in routes

--- src/Routes.js
+++ src/Routes.js
@@ -1,32 +1,34 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+import asyncComponent from "./components/AsyncComponent";
 
-import Home from "./containers/Home";
-import Notes from "./containers/Notes";
-import Login from "./containers/Login";
-import Signup from "./containers/Signup";
-import ResetPassword from "./containers/ResetPassword";
-import ChangePassword from "./containers/ChangePassword";
-import ChangeEmail from "./containers/ChangeEmail";
-import NewNote from "./containers/NewNote";
-import NotFound from "./containers/NotFound";
 import AppliedRoute from "./components/AppliedRoute";
 import AuthenticatedRoute from "./components/AuthenticatedRoute";
 import UnauthenticatedRoute from "./components/UnauthenticatedRoute";
-import Settings from "./containers/Settings";
+
+const AsyncHome = asyncComponent(() => import("./containers/Home"));
+const AsyncNotes = asyncComponent(() => import("./containers/Notes"));
+const AsyncLogin = asyncComponent(() => import("./containers/Login"));
+const AsyncSignup = asyncComponent(() => import("./containers/Signup"));
+const AsyncResetPassword = asyncComponent(() => import("./containers/ResetPassword"));
+const AsyncChangePassword = asyncComponent(() => import("./containers/ChangePassword"));
+const AsyncChangeEmail = asyncComponent(() => import("./containers/ChangeEmail"));
+const AsyncNewNote = asyncComponent(() => import("./containers/NewNote"));
+const AsyncNotFound = asyncComponent(() => import("./containers/NotFound"));
+const AsyncSettings = asyncComponent(() => import("./containers/Settings"));
 
 export default ({ childProps }) =>
   <Switch>
-    <AppliedRoute path="/" exact component={Home} props={childProps} />
-    <UnauthenticatedRoute path="/login" exact component={Login} props={childProps} />
-    <UnauthenticatedRoute path="/signup" exact component={Signup} props={childProps} />
-    <AuthenticatedRoute path="/settings" exact component={Settings} props={childProps} />
-    <AuthenticatedRoute path="/notes/new" exact component={NewNote} props={childProps} />
-    <AuthenticatedRoute path="/notes/:id" exact component={Notes} props={childProps} />
-    <UnauthenticatedRoute path="/login/reset" exact component={ResetPassword} props={childProps} />
+    <AppliedRoute path="/" exact component={AsyncHome} props={childProps} />
+    <UnauthenticatedRoute path="/login" exact component={AsyncLogin} props={childProps} />
+    <UnauthenticatedRoute path="/signup" exact component={AsyncSignup} props={childProps} />
+    <AuthenticatedRoute path="/settings" exact component={AsyncSettings} props={childProps} />
+    <AuthenticatedRoute path="/notes/new" exact component={AsyncNewNote} props={childProps} />
+    <AuthenticatedRoute path="/notes/:id" exact component={AsyncNotes} props={childProps} />
+    <UnauthenticatedRoute path="/login/reset" exact component={AsyncResetPassword} props={childProps} />
     <AuthenticatedRoute path="/settings/password" exact component={ChangePassword} props={childProps} />
-    <AuthenticatedRoute path="/settings/email" exact component={ChangeEmail} props={childProps} />
+    <AuthenticatedRoute path="/settings/email" exact component={AsyncChangeEmail} props={childProps} />
     { /* Finally, catch all unmatched routes */ }
-    <Route component={NotFound} />
+    <Route component={AsyncNotFound} />
   </Switch>
 
