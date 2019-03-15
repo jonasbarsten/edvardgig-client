75575fd1ea2dbc0752e9a84f19bbd88ba393ae44
Jonas Barsten <jonasbarsten@gmail.com>
2019-03-15 17:20:59 +0100

    Adding settings page

--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+
 import Home from "./containers/Home";
 import Notes from "./containers/Notes";
 import Login from "./containers/Login";
@@ -9,14 +10,17 @@ import NotFound from "./containers/NotFound";
 import AppliedRoute from "./components/AppliedRoute";
 import AuthenticatedRoute from "./components/AuthenticatedRoute";
 import UnauthenticatedRoute from "./components/UnauthenticatedRoute";
+import Settings from "./containers/Settings";
 
 export default ({ childProps }) =>
   <Switch>
     <AppliedRoute path="/" exact component={Home} props={childProps} />
     <UnauthenticatedRoute path="/login" exact component={Login} props={childProps} />
     <UnauthenticatedRoute path="/signup" exact component={Signup} props={childProps} />
+    <AuthenticatedRoute path="/settings" exact component={Settings} props={childProps} />
     <AuthenticatedRoute path="/notes/new" exact component={NewNote} props={childProps} />
     <AuthenticatedRoute path="/notes/:id" exact component={Notes} props={childProps} />
     { /* Finally, catch all unmatched routes */ }
     <Route component={NotFound} />
-  </Switch>;
+  </Switch>
+
